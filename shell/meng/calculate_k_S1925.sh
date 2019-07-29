46a7cf6e6f4276dceeaba8efbe01311043eda56f
ruiyang <ruiyang@dell.com>
2019-07-30 00:50:32 +0800

    fix shell path

--- shell/meng/calculate_k_S1925.sh
+++ shell/meng/calculate_k_S1925.sh
@@ -2,13 +2,13 @@
 # name_dataset, radius, k_neighbor, num_class
 # generate only k_neighbor dataset_array of S1925.
 
-python calculate_neighbor.py S1925 50 30 5
-python calculate_neighbor.py S1925 50 40 5
-python calculate_neighbor.py S1925 50 50 5
-python calculate_neighbor.py S1925 50 60 5
-python calculate_neighbor.py S1925 50 70 5
-python calculate_neighbor.py S1925 50 80 5
-python calculate_neighbor.py S1925 50 90 5
-python calculate_neighbor.py S1925 50 100 5
-python calculate_neighbor.py S1925 50 110 5
-python calculate_neighbor.py S1925 50 120 5
+python src/calculate_neighbor.py S1925 50 30 5
+python src/calculate_neighbor.py S1925 50 40 5
+python src/calculate_neighbor.py S1925 50 50 5
+python src/calculate_neighbor.py S1925 50 60 5
+python src/calculate_neighbor.py S1925 50 70 5
+python src/calculate_neighbor.py S1925 50 80 5
+python src/calculate_neighbor.py S1925 50 90 5
+python src/calculate_neighbor.py S1925 50 100 5
+python src/calculate_neighbor.py S1925 50 110 5
+python src/calculate_neighbor.py S1925 50 120 5
