a2bb67dc02d8a9cdb2a2bb3ab0d899cdf05e0170
ruiyang <ruiyang@manjaro.com>
2019-10-04 20:33:31 +0800

    add seq2pdb_main

--- src/pdb2seq_main.py
+++ src/pdb2seq_main.py
@@ -17,7 +17,7 @@ aa_dict = {'Ala': 'A', 'Arg': 'R', 'Asn': 'N', 'Asp': 'D', 'Cys': 'C',
            'Gln': 'Q', 'Glu': 'E', 'Gly': 'G', 'His': 'H', 'Ile': 'I',
            'Leu': 'L', 'Lys': 'K', 'Met': 'M', 'Phe': 'F', 'Pro': 'P',
            'Ser': 'S', 'Thr': 'T', 'Trp': 'W', 'Tyr': 'Y', 'Val': 'V'} # from wiki
-def pdb2seq(seqname, filename, mdlid, chainid, wtflag, position='0', mtaa = '0'):
+def pdb2seq(seqname, filename, mdlid, chainid, wtflag, position='0', wtaa = '0',mtaa = '0'):
     lst = []
     parser = PDBParser(PERMISSIVE=1)
     structure = parser.get_structure(filename,'../datasets/S2648/pdbS2648/' + filename + '.pdb')
@@ -49,9 +49,9 @@ def pdb2seq(seqname, filename, mdlid, chainid, wtflag, position='0', mtaa = '0')
     # print(len(set(lst)))
     fasta_name = '%s.fasta'%seqname
     g = open(fasta_name, 'w+')
-    g.writelines('>%s.fasta|user:sry|date:%s|mdl:%s|chain:%s|pos:%s|mt_res:%s\n'
-                 %(seqname[-12:], time.strftime("%a %b %d %H:%M:%S %Y",time.localtime()),
-                   mdlid, chainid, position, mtaa))
+    g.writelines('>%s.fasta|user:sry|date:%s|mdl:%s|chain:%s|pos:%s|wt_res:%s|mt_res:%s\n'
+                 %(seqname[-14:], time.strftime("%a %b %d %H:%M:%S %Y",time.localtime()),
+                   mdlid, chainid, position, wtaa, mtaa))
     # print(len(lst))
     # print(lst)
     g.writelines(''.join(aa for aa in lst))
@@ -69,18 +69,20 @@ def main():
     for i in range(wt_num):
         wt_index = df_wt.iloc[i, :].values
         filename, chainid = wt_index[0][0:4] , wt_index[1]
-        seqname = '../datasets/%s/seq%s/WT_%s_%04d'%(dataset_name,dataset_name,filename,i+1)
+        seqname = '../datasets/%s/seq%s/WT_%s_%s_%04d'%(dataset_name,dataset_name,filename,chainid,i+1)
         wtflag = 'WT'
         position = '0'
         mtaa = '0'
-        pdb2seq(seqname, filename, mdlid, chainid, wtflag, position, mtaa)
+        wtaa = '0'
+        pdb2seq(seqname, filename, mdlid, chainid, wtflag, position,wtaa, mtaa)
     # =========================================================
     for i in range(mut_num):
         wtflag = 'MT'
-        mt_index = df_mutation.loc[i, ['PDB','CHAIN','POSITION', 'MUTANT']].values
-        filename, chainid, position, mtaa = mt_index[0][0:4], mt_index[1], mt_index[2], mt_index[3]
-        seqname = '../datasets/%s/seq%s/MT_%s_%04d'%(dataset_name,dataset_name,filename,i+1)
-        pdb2seq(seqname, filename, mdlid, chainid, wtflag, position, mtaa)
+        mt_index = df_mutation.loc[i, ['PDB','WILD_TYPE','CHAIN','POSITION', 'MUTANT']].values
+        filename, wtaa, chainid, position, mtaa = mt_index[0][0:4], mt_index[1], mt_index[2], mt_index[3], mt_index[4]
+        seqname = '../datasets/%s/seq%s/MT_%s_%s_%s_%s_%s_%04d'\
+                  %(dataset_name,dataset_name,filename,wtaa,chainid,position,mtaa,i+1)
+        pdb2seq(seqname, filename, mdlid, chainid, wtflag, position, wtaa, mtaa)
 
 if __name__ == '__main__':
     main()
