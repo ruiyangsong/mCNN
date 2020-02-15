70fbb368430c3004b52268d17cc6cb56ec2faf66
unknown <dell@win10.com>
2020-02-15 23:44:15 +0800

    rm -r -cached src_former

--- src_former/shell/generate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# generate datasets_array
-
-python generate_dataset.py S2648 12 0 5 && python calculate_neighbor.py S2648 13 0 && python generate_dataset.py S2648 13 0 5 && python calculate_neighbor.py S2648 14 0 && python generate_dataset.py S2648 14 0 5 && python calculate_neighbor.py S2648 15 0 && python generate_dataset.py S2648 15 0 5 
-# echo "1" && echo "2" && echo "3"
--- src_former/shell/multi1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-for i in $(seq 1 4)
-do
-python train_model.py S2648 50 120 5 0 5 1.01 0 0
-done
