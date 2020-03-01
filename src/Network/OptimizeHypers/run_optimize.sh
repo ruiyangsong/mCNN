71738bd690e1dda78c367b19cb2b252ba435dbb6
unknown <dell@win10.com>
2020-03-02 01:05:24 +0800

    cuda_full

--- src/Network/OptimizeHypers/run_optimize.sh
+++ src/Network/OptimizeHypers/run_optimize.sh
@@ -1,9 +1,9 @@
 # classifier
-time python classifier.py 120t atpe 100 0 0.01 > classifier_mCNNwildCA120_atpe_eval100_testrepport.log
-time python classifier.py 120v atpe 100 0 0.01 > classifier_mCNNwildCA120_atpe_eval100_valacc.log
+time python classifier.py 120t atpe 100 0 full > classifier_mCNNwildCA120_atpe_eval100_testrepport.log
+time python classifier.py 120v atpe 100 0 full > classifier_mCNNwildCA120_atpe_eval100_valacc.log
 
 # regressor
-time python regressor.py 120t atpe 100 0 0.01 > regressor_mCNNwildCA120_atpe_eval100_testreportreg.log
-time python regressor.py 120v atpe 100 0 0.01 regressor_mCNNwildCA120_atpe_eval100_valmae.log
+time python regressor.py 120t atpe 100 0 full > regressor_mCNNwildCA120_atpe_eval100_testreportreg.log
+time python regressor.py 120v atpe 100 0 full > regressor_mCNNwildCA120_atpe_eval100_valmae.log
 
 # multi_task
