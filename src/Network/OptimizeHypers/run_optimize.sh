54e8dcf2ad6e29a888dfb600dd1a0e3b88e95c57
unknown <dell@win10.com>
2020-03-05 17:28:36 +0800

    reverse 1024lab

--- src/Network/OptimizeHypers/run_optimize.sh
+++ src/Network/OptimizeHypers/run_optimize.sh
@@ -1,6 +1,6 @@
 # classifier
-#time python classifier.py 120t atpe 100 0 full > classifier_mCNNwildCA120_atpe_eval100_testrepport.log
-#time python classifier.py 120v atpe 100 0 full > classifier_mCNNwildCA120_atpe_eval100_valacc.log
+time python classifier.py 120t atpe 100 0 full > classifier_mCNNwildCA120_atpe_eval100_testrepport.log
+time python classifier.py 120v atpe 100 0 full > classifier_mCNNwildCA120_atpe_eval100_valacc.log
 
 # regressor
 time python regressor.py 120t atpe 100 0 full > regressor_mCNNwildCA120_atpe_eval100_testreportreg.log
