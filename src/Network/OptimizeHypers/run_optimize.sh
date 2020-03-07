6eda1750a3b654be410abdd789b2f212f951a96a
unknown <dell@win10.com>
2020-03-07 19:34:55 +0800

    sync

--- src/Network/OptimizeHypers/run_optimize.sh
+++ src/Network/OptimizeHypers/run_optimize.sh
@@ -1,9 +1,9 @@
 # classifier
-time python classifier.py 120t atpe 100 0 full > classifier_mCNNwildCA120_atpe_eval100_testrepport.log
+time python classifier.py 120t atpe 100 0 full > classifier_mCNNwildCA120_atpe_eval100_testreportcla.log
 time python classifier.py 120v atpe 100 0 full > classifier_mCNNwildCA120_atpe_eval100_valacc.log
 
 # regressor
 time python regressor.py 120t atpe 100 0 full > regressor_mCNNwildCA120_atpe_eval100_testreportreg.log
-time python regressor.py 120v atpe 100 0 full > regressor_mCNNwildCA120_atpe_eval100_valmae.log
+time python regressor.py 120v tpe 100 0 full > regressor_mCNNwildCA120_tpe_eval100_valmae.log
 
 # multi_task
