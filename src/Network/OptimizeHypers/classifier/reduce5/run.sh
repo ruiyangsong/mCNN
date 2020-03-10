0f7454eb62ab6449e655968b1e8cfdf09dca3b31
unknown <dell@win10.com>
2020-03-11 00:22:11 +0800

    synvc

--- src/Network/OptimizeHypers/classifier/reduce5/run.sh
+++ src/Network/OptimizeHypers/classifier/reduce5/run.sh
@@ -1,2 +1,7 @@
-date && time python classifier.py 120t atpe 100 0 full >> classifier_mCNNwildCA120_atpe_eval100_reduce5_testreportcla.log
-date && time python classifier.py 120v atpe 100 0 full >> classifier_mCNNwildCA120_atpe_eval100_reduce5_valacc.log
+echo 'task 1 begin at:' `date`
+time python ./classifier/reduce5/classifier.py 120t atpe 100 0 full >> ./classifier/reduce5/classifier_mCNNwildCA120_atpe_eval100_reduce5_testreportcla.log
+echo 'task 1 end at:' `date`
+
+echo 'task 2 begin at:' `date`
+time python ./classifier/reduce5/classifier.py 120v atpe 100 0 full >> ./classifier/reduce5/classifier_mCNNwildCA120_atpe_eval100_reduce5_valacc.log
+echo 'task 2 end at:' `date`
--- src/Network/OptimizeHypers/regressor/reduce5/run.sh
+++ src/Network/OptimizeHypers/regressor/reduce5/run.sh
@@ -1,2 +1,7 @@
-date && time python regressor.py 120t atpe 100 0 full >> regressor_mCNNwildCA120_atpe_eval100_reduce5_testreportreg.log
-date && time python regressor.py 120v tpe 100 0 full >> regressor_mCNNwildCA120_tpe_eval100_reduce5_valmae.log
+echo 'task 1 begin at:' `date`
+time python ./regressor/reduce5/regressor.py 120t atpe 100 0 full >> ./regressor/reduce5/regressor_mCNNwildCA120_atpe_eval100_reduce5_testreportreg.log
+echo 'task 1 end at:' `date`
+
+echo 'task 2 begin at:' `date`
+time python ./regressor/reduce5/regressor.py 120v tpe 100 0 full >> ./regressor/reduce5/regressor_mCNNwildCA120_tpe_eval100_reduce5_valmae.log
+echo 'task 2 end at:' `date`
