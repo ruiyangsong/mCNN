ea63af28320ab65691736ccceccad2d8f96a7e7b
unknown <dell@win10.com>
2020-03-12 01:22:15 +0800

    sync

--- src/Network/compare_mut/run.sh
+++ src/Network/compare_mut/run.sh
@@ -9,19 +9,19 @@
 #for neighbor in {30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200}
 for neighbor in {30}
 do
-echo -e '\n----------neighbor'${neighbor} 'Begin neighbor at: ' `date`
+echo -e '\n----------neighbor'$neighbor 'Begin neighbor at: ' `date`
 
 echo -e '\n@classifier begin at: ' `date`
-time python classifier.py ${neighbor} 3 full > classifier.log
+time python classifier.py $neighbor 3 full > classifier.log
 echo -e '\n@classifier end at: ' `date`
 
 echo -e '\n@regressor begin at: ' `date`
-time python regressor.py ${neighbor} 3 full > regressor.log
+time python regressor.py $neighbor 3 full > regressor.log
 echo -e '\n@regressor end at: ' `date`
 
 echo -e '\n@multi_task begin at: ' `date`
-time python multi_task.py ${neighbor} 3 full > multi_task.log
+time python multi_task.py $neighbor 3 full > multi_task.log
 echo -e '\n@multi_task end at: ' `date`
 
-echo -e '\n----------neighbor' ${neighbor} 'End at: ' `date`
+echo -e '\n----------neighbor' $neighbor 'End at: ' `date`
 done
