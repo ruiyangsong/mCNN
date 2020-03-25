8328ecdb886890ba1c662648a0c007485bf71f50
ruiyang <ruiyang@dell.com>
2020-03-25 23:18:34 +0800

    sync

--- src/queueGPU.py
+++ src/queueGPU.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 '--sry,2020/3/12'
-import os,sys,time
+import os,time
 def queueGPU(USER_MEM=10000,INTERVAL=600):
     try:
         totalmemlst=[int(x.split()[2]) for x in os.popen('nvidia-smi -q -d Memory |grep -A4 GPU|grep Total').readlines()]
@@ -8,7 +8,6 @@ def queueGPU(USER_MEM=10000,INTERVAL=600):
     except:
         print('\033[1;35m[WARNING]\nUSER_MEM should smaller than one of the GPU_TOTAL --> %s MiB.\nReset USER_MEM to %s MiB.\033[0m'%(totalmemlst,max(totalmemlst)-1))
         USER_MEM=max(totalmemlst)-1
-        #sys.exit(1)
     while True:
         memlst=[int(x.split()[2]) for x in os.popen('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free').readlines()]
         os.system("echo 'Check at:' `date`")
